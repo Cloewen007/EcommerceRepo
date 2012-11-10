84804c49cbfa11ea9057c64e50dd3d56ee567598
Chris <cloewen007@gmail.com>
2012-11-10 15:14:43 -0600

    Functioning User Authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   
 protected
+
  def require_sudo
     require 'digest'
    authenticate_or_request_with_http_basic do |username, password|
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,7 @@
 class ProductsController < ApplicationController
-  # GET /products
-  # GET /products.json
-  before_filter :require_sudo, :except => [:index, :show]
+
+before_filter :require_sudo, :except =>[:index,:show]
+
   def index
     @products = Product.all
   end
@@ -10,22 +10,12 @@ class ProductsController < ApplicationController
   # GET /products/1.json
   def show
     @product = Product.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @product }
-    end
   end
 
   # GET /products/new
   # GET /products/new.json
   def new
     @product = Product.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @product }
-    end
   end
 
   # GET /products/1/edit
@@ -51,16 +41,13 @@ class ProductsController < ApplicationController
   def update
     @product = Product.find(params[:id])
 
-    respond_to do |format|
       if @product.update_attributes(params[:product])
         format.html { redirect_to @product, notice: 'Product was successfully updated.' }
-        format.json { head :no_content }
       else
         format.html { render action: "edit" }
-        format.json { render json: @product.errors, status: :unprocessable_entity }
       end
     end
-  end
+
 
   # DELETE /products/1
   # DELETE /products/1.json
@@ -68,9 +55,5 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
     @product.destroy
 
-    respond_to do |format|
-      format.html { redirect_to products_url }
-      format.json { head :no_content }
-    end
   end
 end
